18027273211cb9d0de02c9f2ae4833d72f146115
CuiyanWU <79962085+CuiyanWU@users.noreply.github.com>
2025-03-15 02:46:05 -0400

    Update db_structure.sql

--- db_structure.sql
+++ db_structure.sql
@@ -1,33 +1,12 @@
+
 /*
+create schema if not exists septa;
+create schema if not exists phl;
+create schema if not exists census;
 
-This file contains the SQL commands to prepare the database for your queries.
-Before running this file, you should have created your database, created the
-schemas (see below), and loaded your data into the database.
+create extension if not exists postgis;
 
-Creating your schemas
----------------------
-
-You can create your schemas by running the following statements in PG Admin:
-
-    create schema if not exists septa;
-    create schema if not exists phl;
-    create schema if not exists census;
-
-Also, don't forget to enable PostGIS on your database:
-
-    create extension if not exists postgis;
-
-Loading your data
------------------
-
-After you've created the schemas, load your data into the database specified in
-the assignment README.
-
-Finally, you can run this file either by copying it all into PG Admin, or by
-running the following command from the command line:
-
-    psql -U postgres -d <YOUR_DATABASE_NAME> -f db_structure.sql
-
+psql -U postgres -d <YOUR_DATABASE_NAME> -f db_structure.sql
 */
 
 -- Add a column to the septa.bus_stops table to store the geometry of each stop.
