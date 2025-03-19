9515fbec28f6453bbbc5b3f6bf461cf058dc8cb7
SeanMKoh <seankoh154@gmail.com>
2025-03-19 11:56:47 -0400

    completed query10

---
+++ query10.sql
@@ -0,0 +1,34 @@
+with protected as (
+    select geog
+    from phl.bike_network
+    where type like '%Separated Bike Lane%'
+),
+info as (
+    select
+        rail.stop_id,
+        rail.stop_name,
+        rail.stop_lon,
+        rail.stop_lat,
+        case
+            when bike.distance < 500 then 'Proteced bike lane within ' || bike.distance::character || 'm'
+            else
+                'No protected bike lane within 500m'
+        end as stop_desc
+    from septa.rail_stops as rail
+    cross join
+        lateral (
+            select
+                protected.geog,
+                rail.geog <-> protected.geog as distance
+            from protected
+            order by distance
+            limit 1
+        ) as bike
+)
+select 
+    stop_id,
+    stop_name,
+    stop_desc,
+    stop_lon,
+    stop_lat
+from info;
