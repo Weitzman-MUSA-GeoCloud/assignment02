d94aea75c4098c57fcced0154b637e5484f09715
AlbyOnly1 <Christopher.h.chu@gmail.com>
2025-05-12 16:04:10 -0400

    Update query04.sql

--- query04.sql
+++ query04.sql
@@ -7,48 +7,39 @@ find the two routes with the longest trips.
 */
 
 
+
 WITH trip_lengths AS (
     SELECT
         routes.route_short_name,
         trips.trip_headsign,
+        shapes.shape_id,
         public.ST_MakeLine(
-            public.ST_SetSRID(
-                public.ST_MakePoint(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326
-            )
-        ) AS shape_geog,
-        ROUND(public.ST_Length(
-            public.ST_MakeLine(
+            ARRAY_AGG(
                 public.ST_SetSRID(
-                    public.ST_MakePoint(
-                        shapes.shape_pt_lon, shapes.shape_pt_lat
-                    ), 4326
-                )
+                    public.ST_MakePoint(shapes.shape_pt_lon, shapes.shape_pt_lat), 4326
+                ) 
+                ORDER BY shapes.shape_pt_sequence
             )
-        )::INTEGER, 0) AS shape_length,
-        ROW_NUMBER() OVER (
-            ORDER BY SUM(shapes.shape_dist_traveled) DESC
-        ) AS rank
+        ) AS shape_geog
     FROM
-        septa.bus_trips AS trips
-    JOIN
         septa.bus_shapes AS shapes
-        ON
-            trips.shape_id = shapes.shape_id
-    JOIN
+    LEFT JOIN
+        septa.bus_trips AS trips
+        ON shapes.shape_id = trips.shape_id
+    LEFT JOIN
         septa.bus_routes AS routes
-        ON
-            trips.route_id = routes.route_id
+        ON trips.route_id = routes.route_id
     GROUP BY
-        routes.route_short_name, trips.trip_headsign
+        shapes.shape_id, trips.trip_headsign, routes.route_short_name
 )
 SELECT
     route_short_name,
     trip_headsign,
+    shape_id,
     shape_geog,
-    shape_length
+    ROUND(public.ST_Length(shape_geog::public.geography)::NUMERIC, 2) AS shape_length
 FROM
-    trip_lengths
-WHERE
-    rank <= 2
+    busroute_shapes
 ORDER BY
-    shape_length DESC;
+    shape_length DESC
+LIMIT 2;
