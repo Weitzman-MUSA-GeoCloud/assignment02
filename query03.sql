899015dc1db07de2d32b9f43c525b991e2954829
Zhanchao Yang <zhanchao@upenn.edu>
2026-02-17 21:03:35 -0500

    linter

--- query03.sql
+++ query03.sql
@@ -11,12 +11,12 @@ select
     round(st_distance(parcels.geog, stops.geog)::numeric, 2) as distance
 from phl.pwd_parcels as parcels
 cross join
-lateral (
-    select
-        bus_stops.stop_name,
-        bus_stops.geog
-    from septa.bus_stops as bus_stops
-    order by parcels.geog <-> bus_stops.geog
-    limit 1
-) as stops
+    lateral (
+        select
+            bus_stops.stop_name,
+            bus_stops.geog
+        from septa.bus_stops as bus_stops
+        order by parcels.geog <-> bus_stops.geog
+        limit 1
+    ) as stops
 order by distance desc
